4a73b43c5ab70f1af769bdd96edb504434aba693
Renee Sarley <reneesarley@gmail.com>
2018-07-13 12:40:45 -0700

    add client controller file, client view folder, AddNewForm file, and client controller test file

---
+++ HairSalon.Tests/Controllers.Tests/ClientsControllersTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using HairSalon.Models;
+using HairSalon.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HairSalon.Tests.Controllers.Tests
+{
+
+    [TestClass]
+    public class ClientsControllersTests : IDisposable
+
+
+    {
+        public void Dispose()
+        {
+            Stylist.DeleteAll();
+        }
+
+        public ClientsControllersTests()
+        {
+            DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=renee_sarley_test;";
+        }
+
+        [TestMethod]
+        public void AddNewForm_ReturnsCorrectView_View()
+        {
+            //Arrange
+            ClientsController controller = new ClientsController();
+
+            //Act
+            IActionResult addNewFormView = controller.AddNewForm();
+
+            //Assert
+            Assert.IsInstanceOfType(addNewFormView, typeof(ViewResult));
+        }
+    }
+}
---
+++ HairSalon/Controllers/Clients.Controller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HairSalon.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace HairSalon.Controllers
+{
+    public class ClientsController : Controller
+    {
+        [HttpGet ("/clients/new")]
+        public IActionResult AddNewForm()
+        {
+            return View(Stylist.GetAll());
+        }
+    }
+}
