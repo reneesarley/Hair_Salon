fad664677106239eb001f7cbc6bcd55951a83a1c
Renee Sarley <reneesarley@gmail.com>
2018-07-13 08:58:35 -0700

    add database model, connection package and connection method to startup file.

---
+++ HairSalon/Database.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace HairSalon
+{
+    public class DB
+    {
+        public static MySqlConnection Connection()
+        {
+            MySqlConnection conn = new MySqlConnection(DBConfiguration.ConnectionString);
+            return conn;
+        }
+    }
+}
--- HairSalon/Startup.cs
+++ HairSalon/Startup.cs
@@ -57,4 +57,8 @@ namespace HairSalon
             });
         }
     }
+    public static class DBConfiguration
+    {
+        public static string ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=renee_sarley;";
+    }
 }
