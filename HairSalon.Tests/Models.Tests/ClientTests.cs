307e7d17f7d4b75befe57a62e030089ece7c3d33
Renee Sarley <reneesarley@gmail.com>
2018-07-13 11:57:38 -0700

    add client model and test files

---
+++ HairSalon.Tests/Models.Tests/ClientTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using HairSalon.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HairSalon.Tests.Models.Tests
+{
+   
+    [TestClass]
+    public class ClientTests : IDisposable
+    {
+        public void Dispose()
+        {
+            Client.DeleteAll();
+        }
+
+        public ClientTests()
+        {
+            DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=renee_sarley_test;";
+        }
+
+        //[TestMethod]
+        //public ClientTests()
+        //{
+        //}
+    }
+}
---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace HairSalon.Models
+{
+    public class Client
+    {
+        private int _id;
+        private string _firstName;
+        private string _lastName;
+        private int _stylistId;
+
+
+        public Client(string firstName, string lastName, int stylistId, int id = 0)
+        {
+            _id = id;
+            _firstName = firstName;
+            _lastName = lastName;
+            _stylistId = stylistId;
+        }
+
+        public string GetFirstName()
+        {
+            return _firstName;
+        }
+
+        public string GetLastName()
+        {
+            return _lastName;
+        }
+        public int GetId()
+        {
+            return _id;
+        }
+        public int GetStylistId()
+        {
+            return _stylistId;
+        }
+
+    }
+}
