ea4e8ce6c36f693179e066e35aa7853c819cc55e
Renee Sarley <reneesarley@gmail.com>
2018-07-13 12:04:12 -0700

    add DeleteAll and overide equals methods to client class

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using MySql.Data.MySqlClient;
 
 namespace HairSalon.Models
 {
@@ -40,6 +41,46 @@ namespace HairSalon.Models
         {
             return _stylistId;
         }
+
+        public static void DeleteAll()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"DELETE FROM clients;";
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        public override bool Equals(System.Object otherClient)
+        {
+            if (!(otherClient is Client))
+            {
+                return false;
+            }
+            else
+            {
+                Client newClient = (Client)otherClient;
+                bool firstNameEquality = this.GetFirstName().Equals(newClient.GetFirstName());
+                bool lastNameEquality = this.GetLastName().Equals(newClient.GetLastName());
+                bool idEquality = this.GetId().Equals(newClient.GetId());
+                bool stylistIdEquality = this.GetStylistId().Equals(newClient.GetStylistId());
+                return (firstNameEquality && lastNameEquality && idEquality && stylistIdEquality);
+            }
+        }
+
+        //public override int GetHashCode()
+        //{
+        //    return this.GetFirstName().GetHashCode();
+
+        //}
 
     }
 }
