215bbd82c0faad3fba79be8533363cf6f8210828
Student <example@example.com>
2018-07-14 13:15:14 -0700

    add services link to home index

--- HairSalon.Tests/Controllers.Tests/ServicesControllerTests.cs
+++ HairSalon.Tests/Controllers.Tests/ServicesControllerTests.cs
@@ -1,10 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using HairSalon.Models;
+using HairSalon.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+
 namespace HairSalon.Tests.Controllers.Tests
 {
+    [TestClass]
     public class ServicesControllerTests
     {
+        public void Dispose()
+        {
+            Service.DeleteAll();
+        }
+
         public ServicesControllerTests()
         {
+            DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=renee_sarley_test;";
         }
+
+        [TestMethod]
+        public void Index_ReturnsCorrectView_View()
+        {
+            //Arrange
+           ServicesController controller = new ServicesController();
+
+            //Act
+            IActionResult indexView = controller.Index();
+
+            //Assert
+            Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+        }
+
     }
 }
