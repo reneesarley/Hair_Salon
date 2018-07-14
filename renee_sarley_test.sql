b9d34f1691f93459400748ff64dde5787aef8c67
Student <example@example.com>
2018-07-14 14:07:42 -0700

    udpate sql files

--- renee_sarley_test.sql
+++ renee_sarley_test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jul 13, 2018 at 11:19 PM
+-- Generation Time: Jul 14, 2018 at 09:05 PM
 -- Server version: 5.6.38
 -- PHP Version: 7.2.1
 
@@ -38,6 +38,17 @@ CREATE TABLE `clients` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `services`
+--
+
+CREATE TABLE `services` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `serviceName` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `stylists`
 --
 
@@ -47,6 +58,38 @@ CREATE TABLE `stylists` (
   `lastName` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stylists_services`
+--
+
+CREATE TABLE `stylists_services` (
+  `id` int(11) NOT NULL,
+  `stylist_id` int(11) NOT NULL,
+  `service_id` int(11) NOT NULL,
+  `cost` decimal(10,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `stylists_services`
+--
+
+INSERT INTO `stylists_services` (`id`, `stylist_id`, `service_id`, `cost`) VALUES
+(1, 22, 12, '0'),
+(2, 23, 13, '0'),
+(3, 24, 15, '0'),
+(4, 27, 18, '0'),
+(5, 28, 19, '0'),
+(6, 31, 22, '0'),
+(7, 32, 23, '0'),
+(8, 35, 26, '0'),
+(9, 36, 27, '0'),
+(10, 39, 30, '0'),
+(11, 40, 31, '0'),
+(12, 43, 34, '0'),
+(13, 44, 35, '0');
+
 --
 -- Indexes for dumped tables
 --
@@ -55,12 +98,27 @@ CREATE TABLE `stylists` (
 -- Indexes for table `clients`
 --
 ALTER TABLE `clients`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
 
 --
 -- Indexes for table `stylists`
 --
 ALTER TABLE `stylists`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `stylists_services`
+--
+ALTER TABLE `stylists_services`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -71,13 +129,25 @@ ALTER TABLE `stylists`
 -- AUTO_INCREMENT for table `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `services`
+--
+ALTER TABLE `services`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `stylists`
 --
 ALTER TABLE `stylists`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+
+--
+-- AUTO_INCREMENT for table `stylists_services`
+--
+ALTER TABLE `stylists_services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
