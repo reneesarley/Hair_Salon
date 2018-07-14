3a32838b1f50e36bbc95f7770766cef12b8483eb
Student <example@example.com>
2018-07-14 11:10:24 -0700

    add service model and test files

---
+++ HairSalon.Tests/Models.Tests/ServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using HairSalon.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace HairSalon.Tests.Models.Tests
+                   
+{
+    [TestClass]
+    public class ServiceTests : IDisposable
+    {
+
+        public void Dispose()
+        {
+            Service.DeleteAll();
+        }
+
+        public ServiceTests()
+        {
+            DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=renee_sarley_test;";
+        }
+
+        [TestMethod]
+        public void GetAll_DBStartsEmpty_0()
+        {
+            //Arrange
+            //Act
+            int result = Service.GetAll().Count;
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Save_SavesToDatabase_ServiceList()
+        {
+            //Arrange
+            Service testService = new Service("Cut - Long Hair", 2);
+
+            //Act
+            testService.Save();
+            List<Service> result = Service.GetAll();
+            List<Service> testService = new List<Service>() { testService };
+
+            //Assert
+            CollectionAssert.AreEqual(result, testService);
+        }
+    }
+}
---
+++ HairSalon/Models/Service.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using MySql.Data.MySqlClient;
+
+
+namespace HairSalon.Models
+{
+    public class Service
+    {
+        private int _id;
+        private string _serviceName;
+
+        public Service(string serviceName, int id = 0)
+        {
+            _id = id;
+            _serviceName = serviceName;
+        }
+
+        public void Save()
+        {
+            //MySqlConnection conn = DB.Connection();
+            //conn.Open();
+
+            //var cmd = conn.CreateCommand() as MySqlCommand;
+            //cmd.CommandText = @"INSERT INTO clients (firstName, lastName, stylistId) VALUES (@FirstName, @Lastname, @StylistId);";
+
+            //MySqlParameter firstName = new MySqlParameter();
+            //firstName.ParameterName = "@FirstName";
+            //firstName.Value = _firstName;
+            //cmd.Parameters.Add(firstName);
+
+            //MySqlParameter lastName = new MySqlParameter();
+            //lastName.ParameterName = "@LastName";
+            //lastName.Value = _lastName;
+            //cmd.Parameters.Add(lastName);
+
+            //MySqlParameter stylistId = new MySqlParameter();
+            //stylistId.ParameterName = "@StylistId";
+            //stylistId.Value = _stylistId;
+            //cmd.Parameters.Add(stylistId);
+
+            //cmd.ExecuteNonQuery();
+            //_id = (int)cmd.LastInsertedId;
+
+            //conn.Close();
+            //if (conn != null)
+            //{
+            //    conn.Dispose();
+            //}
+
+        }
+
+        public static List<Service> GetAll()
+        {
+            //List<Client> allClients = new List<Client> { };
+
+            //MySqlConnection conn = DB.Connection();
+            //conn.Open();
+
+            //var cmd = conn.CreateCommand() as MySqlCommand;
+            //cmd.CommandText = @"SELECT * FROM clients;";
+
+            //MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+            //while (rdr.Read())
+            //{
+            //    int id = rdr.GetInt32(0);
+            //    string firstName = rdr.GetString(1);
+            //    string lastName = rdr.GetString(2);
+            //    int stylistId = rdr.GetInt32(3);
+            //    Client newClient = new Client(firstName, lastName, stylistId, id);
+            //    allClients.Add(newClient);
+            //}
+
+            //conn.Close();
+            //if (conn != null)
+            //{
+            //    conn.Dispose();
+            //}
+
+            //return allClients;
+
+        }
+
+
+        public static void DeleteAll()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"DELETE FROM services;";
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+    }
+}
